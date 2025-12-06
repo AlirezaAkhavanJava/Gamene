e16804e997d2322b1ae749a36ff3f6a8f36d5687
alireza <theethanwardhere@gmail.com>
2025-12-06 11:53:26 +0330

    Profiles added (for practice)

--- src/main/java/com/arcade/gamon/ApplicationConfig.java
+++ src/main/java/com/arcade/gamon/ApplicationConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class ApplicationConfig { /*Configuration class for the bean container ! */
--- src/main/java/com/arcade/gamon/GamonApplication.java
+++ src/main/java/com/arcade/gamon/GamonApplication.java
@@ -2,18 +2,26 @@ package com.arcade.gamon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+
+import java.util.Collections;
 
 @SpringBootApplication
 public class GamonApplication {
     static void main(String[] args) {
 
-        var context = SpringApplication.run(GamonApplication.class, args);
+//        var context = SpringApplication.run(GamonApplication.class, args);
+//
+
+        SpringApplication app = new SpringApplication(GamonApplication.class);
+        app.setDefaultProperties(Collections.singletonMap("spring.profiles.active", "dev"));
+        ApplicationContext context = app.run(args);
 
         UnitedStates unitedStates = context.getBean(UnitedStates.class);
         System.out.println(unitedStates.whatToDo());
-    }
 
+    }
 
 
 }
