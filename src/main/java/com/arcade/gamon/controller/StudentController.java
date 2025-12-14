81dc5e45238a41001091e785b99307aabf0ea97f
alireza <theethanwardhere@gmail.com>
2025-12-14 09:25:45 +0330

    before testing

--- src/main/java/com/arcade/gamon/controller/StudentController.java
+++ src/main/java/com/arcade/gamon/controller/StudentController.java
@@ -3,10 +3,16 @@ package com.arcade.gamon.controller;
 import com.arcade.gamon.Service.StudentService;
 import com.arcade.gamon.domin.dto.StudentDto;
 import com.arcade.gamon.domin.dto.StudentResponseDto;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -16,31 +22,48 @@ public class StudentController {
 
     private final StudentService studentService;
 
-
+    // ================= REGISTER NEW STUDENT=========================
     @PostMapping("public/new")
-    public StudentResponseDto save(@RequestBody StudentDto studentDto) {
+    public StudentResponseDto save(@Valid @RequestBody StudentDto studentDto) {
         return studentService.saveStudent(studentDto);
     }
 
+    // ================= GET ALL THE STUDENTS ========================
     @GetMapping("private/all")
     public List<StudentResponseDto> findAll() {
         return studentService.getAll();
     }
 
+    // =================== GET STUDENT BY ID ==========================
     @GetMapping("/private/id/{id}")
     public StudentResponseDto findById(@PathVariable int id) {
         return studentService.getById(id);
     }
 
+    // =================== GET STUDENTS BY NAME ========================
     @GetMapping("private/{name}")
     public List<StudentResponseDto> findByFirstNameContainingIgnoreCase(@PathVariable("name") String firstName) {
         return studentService.getStudentsByFirstName(firstName);
     }
 
+    // =================== REMOVE STUDENT BY ID =======================
     @DeleteMapping("private/remove/{id}")
     @ResponseStatus(HttpStatus.OK)
     public void deleteById(@PathVariable Integer id) {
         studentService.removeStudentById(id);
+    }
+
+    // =================== VALIDATION HANDLER =======================
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleMethodArgumentNotValid(MethodArgumentNotValidException exception) {
+        var errors = new HashMap<String, String>();
+        exception.getBindingResult().getAllErrors().forEach(error -> {
+            var filedName = ((FieldError) error).getField();
+            var message = error.getDefaultMessage();
+            errors.put(filedName, message);
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
 
--- src/main/java/com/arcade/gamon/domin/dto/StudentDto.java
+++ src/main/java/com/arcade/gamon/domin/dto/StudentDto.java
@@ -1,9 +1,19 @@
 package com.arcade.gamon.domin.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
 public record StudentDto(
+        @NotNull
+        @NotEmpty(message = "Firstname can not be empty !")
         String firstName,
+
+        @NotNull
+        @NotEmpty(message = "Lastname is Required !")
         String lastName,
+
         String email,
+
         Integer schoolId
 ) {
 }
